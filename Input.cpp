6a548ca76d9fce07293800a162b34095810018bf
karlkkxu <kim.karlsson@outlook.com>
2020-03-16 14:00:09 +0200

    Näppäinten seuranta (ei vielä tee mitään)

--- Input.cpp
+++ Input.cpp
@@ -2,27 +2,33 @@
 
 void Input::beginFrame()
 {
+	this->pressedKeys.clear();
+	this->releasedKeys.clear();
 }
 
 void Input::keyUpEvent(SDL_Event& event)
 {
+	this->releasedKeys[event.key.keysym.scancode] = true;
+	this->heldKeys[event.key.keysym.scancode] = false;
 }
 
 void Input::keyDownEvent(SDL_Event& event)
 {
+	this->pressedKeys[event.key.keysym.scancode] = true;
+	this->heldKeys[event.key.keysym.scancode] = true;
 }
 
 bool Input::keyPressed(SDL_Scancode key)
 {
-	return false;
+	return pressedKeys[key];
 }
 
 bool Input::keyReleased(SDL_Scancode key)
 {
-	return false;
+	return releasedKeys[key];
 }
 
 bool Input::keyHeld(SDL_Scancode key)
 {
-	return false;
+	return heldKeys[key];
 }
