78bce44a8becde547fc1289f247ad7184c9a9c9a
karlkkxu <kim.karlsson@outlook.com>
2020-03-17 22:29:56 +0200

    MainGameLoop asetettu tutkimaan näppäimiä, escape sulkee ohjelman

--- MainGame.cpp
+++ MainGame.cpp
@@ -1,10 +1,10 @@
 #include "SDL.h"
 #include "MainGame.h"
 #include "Graphics.h"
+#include "Input.h"
 
 MainGame::MainGame() {
 	SDL_Init(SDL_INIT_EVERYTHING);
-	Graphics graphics(1024, 768);
 }
 
 MainGame::~MainGame() {
@@ -14,8 +14,24 @@ MainGame::~MainGame() {
 void MainGame::play()
 {
 	SDL_Event event;
+	Graphics graphics(1024, 768);
+	Input input;
 
 	while (true) {
+		input.beginFrame();
+		
+		if (SDL_PollEvent(&event)) {
+
+			if (event.type == SDL_KEYDOWN && event.key.repeat == 0)
+				input.keyDownEvent(event);
+
+			else if (event.type == SDL_KEYUP)
+				input.keyUpEvent(event);
+
+			if (input.keyPressed(SDL_SCANCODE_ESCAPE))
+				return;
+
+		}
 
 	}
 }
